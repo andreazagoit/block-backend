f724384cdd3c7262a423c007241040eba7deec29
Andrea Zago <info@andreazago.it>
2023-07-02 15:48:29 +0200

    feat: added cors

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 
 const app: Express = express();
 const port = process.env.PORT || 4000;
 
+app.use(cors());
+
 app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    data: [],
+  });
+});
+
+app.get("/add", (req: Request, res: Response) => {
   res.status(200).json({
     data: [],
   });
